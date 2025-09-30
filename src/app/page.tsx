cd14d7157fe1d7bae5ee67b41fa3844e284b3d9a
KIM <tjdghks7373@naver.com>
2025-09-30 17:09:41 +0900

    메인수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
-import styles from "./page.module.css";
+import Header from '@/components/layout/Header';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  max-width: 1400px;
+  margin: 0 auto;
+  padding-left: 20px;
+  padding-right: 20px;
+`;
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      김성환 천재<br/>
-      김진숙 바보
+    <div>
+      <Header />
+      <Container>121212</Container>
     </div>
   );
 }
