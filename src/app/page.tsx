865d837abbf388beef9a8b89d2c36e4596bf8070
KIM <tjdghks7373@naver.com>
2025-09-30 17:43:18 +0900

    메인수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,3 @@
-import Header from '@/components/layout/Header';
-import styled from 'styled-components';
-
-const Container = styled.div`
-  max-width: 1400px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-`;
-
 export default function Home() {
-  return (
-    <div>
-      <Header />
-      <Container>121212</Container>
-    </div>
-  );
+  return <div>121212</div>;
 }
