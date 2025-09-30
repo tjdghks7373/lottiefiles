3783c2a1cf046ec372a60a53995d67fae3a51d77
KIM <tjdghks7373@naver.com>
2025-09-30 17:59:50 +0900

    메인수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import Header from '@/components/layout/Header';
+import dynamic from "next/dynamic";
 import styled from 'styled-components';
+
+const Header = dynamic(() => import('@/components/layout/Header'), { ssr: false });
 
 const Container = styled.div`
   max-width: 1400px;
   margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
+  padding: 0 20px;
 `;
 
 export default function Home() {
