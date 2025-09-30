c2db49bcdef82bb8656188c4c7acf635e7dfc0c8
KIM <tjdghks7373@naver.com>
2025-09-30 17:49:35 +0900

    메인수정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  compiler: {
+    styledComponents: true
+  }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import Header from '@/components/layout/Header';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  max-width: 1400px;
+  margin: 0 auto;
+  padding-left: 20px;
+  padding-right: 20px;
+`;
+
 export default function Home() {
-  return <div>121212</div>;
+  return (
+    <div>
+      <Header />
+      <Container>121212</Container>
+    </div>
+  );
 }
